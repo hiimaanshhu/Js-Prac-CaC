ca1521d8c0ab7620a6ff8bf9c47c94c278230c5d
Himanshu Mishra <161751114+hiimaanshhu@users.noreply.github.com>
2024-09-02 16:59:08 +0000

    prac done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144354 //constant value
+let accountEmail = "himanshu123@gmail.com" //let is the imp one. 
+var accountPassword = "12345" //var not to use.
+accountCity ="Jaipur" //code will run but this is a wrong method to write.
+let accountState;
+
+//------------------
+
+accountEmail = "him.com"
+accountPassword = "32332323"
+accountCity= "pune"
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
